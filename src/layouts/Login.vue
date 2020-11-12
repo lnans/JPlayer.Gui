ee4c3e58199c00c0ec92ff20714bd7399f9bb5e2
Laupretre Nans <nans.laupretre@gmail.com>
2020-11-12 22:08:29 +0100

    Create Login page

--- src/layouts/Login.vue
+++ src/layouts/Login.vue
@@ -1,8 +1,39 @@
 <template>
-    <v-app id="login">
+    <v-app>
         <v-main>
-            <div>
-                Login
+            <div id="login">
+                <div class="login-container">
+                    <p class="login__title">{{ $t('login.title') }} </p>
+                    <p class="login__subtitle"> {{ $t('login.forgot_password') }} <br /> {{ $t('login.contact_admin') }} </p>
+                    <v-form v-model="valid" :disabled="loading">
+                        <v-text-field
+                            outlined
+                            v-model="loginForm.userName"
+                            :label="$t('login.username')"
+                            append-icon="fa-user"
+                            :rules="[rules.required]">
+                        </v-text-field>
+                        <v-text-field
+                            outlined
+                            v-model="loginForm.password"
+                            :label="$t('login.password')"
+                            :append-icon="showPwd ? 'fa-eye' : 'fa-eye-slash'"
+                            :type="showPwd ? 'text' : 'password'"
+                            :rules="[rules.required]"
+                            @click:append="showPwd = !showPwd">
+                        </v-text-field>
+                        <v-btn
+                            large
+                            class="login__btn"
+                            color="primary"
+                            width="100%"
+                            :disabled="!valid"
+                            :loading="loading"
+                            @click="submit()">
+                            {{ $t('login.login_button') }}
+                        </v-btn>
+                    </v-form>
+                </div>
             </div>
         </v-main>
     </v-app>
@@ -10,11 +41,58 @@
 
 <script>
 export default {
-    name: 'Login'
+    name: 'Login',
+    data () {
+        return {
+            valid: false,
+            showPwd: false,
+            loading: false,
+            loginForm: {
+                userName: "",
+                password: ""
+            },
+            rules: {
+                required: value => !!value || this.$t('rules.required')
+            }
+        }
+    },
+    methods: {
+        submit () {
+            this.loading = true
+        }
+    }
 }
 </script>
+
 <style lang="scss" scoped>
 #login {
     background-color: #C8C8C8;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.login-container {
+    background-color: #FFFFFF;
+    filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.25));
+    width: 380px;
+    height: 410px;
+    border-radius: 5px;
+    padding: 35px 35px 50px 35px;
+    .login__title {
+        font-weight: bold;
+        font-size: 36px;
+        line-height: 43px;
+        margin-bottom: 0;
+    }
+    .login__subtitle {
+        font-size: 14px;
+        line-height: 17px;
+        margin-bottom: 30px;
+    }
+    .login__btn {
+        text-transform: none;
+    }
 }
 </style>
