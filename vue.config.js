be872cdd858ee8dde85b1ce5bd009819837672e5
Laupretre Nans <nans.laupretre@gmail.com>
2020-11-14 12:06:28 +0100

    Https dev config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,19 @@
+const fs = require("fs");
+
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
+    devServer: {
+        host: "localhost",
+        https: {
+            key: fs.readFileSync("./certs/localhost-key.pem"),
+            cert: fs.readFileSync("./certs/localhost.pem")
+        }
+    },
+    pluginOptions: {
+        i18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            localeDir: 'locales',
+            enableInSFC: false
+        }
     }
-  }
 }
