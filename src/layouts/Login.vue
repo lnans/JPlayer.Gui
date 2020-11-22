292df41173eaa471836bef7839bd2b144718958f
Laupretre Nans <nans.laupretre@gmail.com>
2020-11-22 18:56:04 +0100

    Rework Login page with responsive design

--- src/layouts/Login.vue
+++ src/layouts/Login.vue
@@ -1,52 +1,45 @@
 <template>
     <v-app>
         <v-main>
-            <v-container fluid id="app_root" class="d-flex justify-center">
-                <v-card class="col-2 align-self-center">
-                    <v-card-title class="text-h4">{{ $t('login.title') }}</v-card-title>
-                    <v-card-subtitle>{{ $t('login.forgot_password') }} <br /> {{ $t('login.contact_admin') }}</v-card-subtitle>
+            <v-container fluid id="app_root" v-bind:class="`d-flex d-${breakpoint}-flex flex-column justify-center justify-${breakpoint}-center`">
+                <v-card v-bind:class="`login-card col-3 col-${breakpoint}-3 align-self-center align-self-${breakpoint}-center`" :dark="$vuetify.theme.isDark">
+                    <v-card-title>
+                        <p v-bind:class="`text-center text-h4 text-${breakpoint}-h4 col-12 col-${breakpoint}-12 ma-0`">{{ $t('app_name') }}</p>
+                    </v-card-title>
                     <v-card-text>
-                        <v-text-field
-                            outlined
-                            v-model="loginForm.login"
-                            :label="$t('login.username')"
-                            append-icon="fa-user"
-                            :rules="[rules.required]">
-                        </v-text-field>
+                        <v-form ref="loginForm" v-model="valid" :disabled="loading">
+                            <v-text-field
+                                outlined
+                                v-model="loginForm.login"
+                                :label="$t('login.username')"
+                                append-icon="fa-user"
+                                :rules="[rules.required]"
+                                @keyup.enter="submit()">
+                            </v-text-field>
+                            <v-text-field
+                                outlined
+                                v-model="loginForm.password"
+                                :label="$t('login.password')"
+                                :append-icon="showPwd ? 'fa-eye' : 'fa-eye-slash'"
+                                :type="showPwd ? 'text' : 'password'"
+                                :rules="[rules.required]"
+                                @click:append="showPwd = !showPwd"
+                                @keyup.enter="submit()">
+                            </v-text-field>
+                            <p>{{ $t('login.forgot_password') }} <br /> {{ $t('login.contact_admin') }}</p>
+                            <v-btn
+                                large
+                                class="login__btn"
+                                color="primary"
+                                width="100%"
+                                :disabled="!valid"
+                                :loading="loading"
+                                @click="submit()">
+                                {{ $t('login.login_button') }}
+                            </v-btn>
+                        </v-form>
                     </v-card-text>
                 </v-card>
-                <!-- <div class="login-container align-self-center">
-                    <p class="login__title">{{ $t('login.title') }} </p>
-                    <p class="login__subtitle"> {{ $t('login.forgot_password') }} <br /> {{ $t('login.contact_admin') }} </p>
-                    <v-form v-model="valid" :disabled="loading">
-                        <v-text-field
-                            outlined
-                            v-model="loginForm.login"
-                            :label="$t('login.username')"
-                            append-icon="fa-user"
-                            :rules="[rules.required]">
-                        </v-text-field>
-                        <v-text-field
-                            outlined
-                            v-model="loginForm.password"
-                            :label="$t('login.password')"
-                            :append-icon="showPwd ? 'fa-eye' : 'fa-eye-slash'"
-                            :type="showPwd ? 'text' : 'password'"
-                            :rules="[rules.required]"
-                            @click:append="showPwd = !showPwd">
-                        </v-text-field>
-                        <v-btn
-                            large
-                            class="login__btn"
-                            color="primary"
-                            width="100%"
-                            :disabled="!valid"
-                            :loading="loading"
-                            @click="submit()">
-                            {{ $t('login.login_button') }}
-                        </v-btn>
-                    </v-form>
-                </div> -->
             </v-container>
             <Toaster />
         </v-main>
@@ -72,8 +65,17 @@ export default {
             }
         }
     },
+    computed: {
+        breakpoint () {
+            return this.$vuetify.breakpoint.name
+        }
+    },
     methods: {
         submit () {
+            if (!this.valid) {
+                this.$refs.loginForm.validate()
+                return
+            }
             this.loading = true
             this.$http.post(`auth/signin`, this.loginForm)
                 .then((response) => {
@@ -92,30 +94,9 @@ export default {
 
 <style lang="scss" scoped>
 #app_root {
-    // background-color: $app-default-bg-color;
     height: 100%;
-    max-height: 100%;
 }
-.login-container {
-    background-color: #FFFFFF;
-    filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.25));
-    width: 380px;
-    height: 410px;
-    border-radius: 5px;
-    padding: 35px 35px 50px 35px;
-    .login__title {
-        font-weight: bold;
-        font-size: 36px;
-        line-height: 43px;
-        margin-bottom: 0;
-    }
-    .login__subtitle {
-        font-size: 14px;
-        line-height: 17px;
-        margin-bottom: 30px;
-    }
-    .login__btn {
-        text-transform: none;
-    }
+.login-card {
+    min-width: 250px;
 }
 </style>
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -7,58 +7,65 @@ axios.defaults.baseURL = process.env.VUE_APP_API_URL
 axios.defaults.withCredentials = true
 
 let config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  // timeout: 60 * 1000, // Timeout
+    // baseURL: process.env.baseURL || process.env.apiUrl || ""
+    // timeout: 60 * 1000, // Timeout
 };
 
 const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
-    return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
+    function(config) {
+        // Do something before request is sent
+        return config;
+    },
+    function(error) {
+        // Do something with request error
+        return Promise.reject(error);
+    }
 );
 
 // Add a response interceptor
 _axios.interceptors.response.use(
-  function(response) {
-    return response.data;
-  },
-  function(error) {
-    // Do something with response error
-    // In case of 401, redirect to login page
-    // If the error come from the login page, display the error
-    const errCode = error.response.data.error;
-    if(error.response.status === 401 && errCode === 'authNotAuthenticated') {
-        window.location = process.env.VUE_APP_LOGIN_PAGE
-    } else {
-        Vue.prototype.$toaster.error(error.response.data.error)
-    }
+    function(response) {
+        return response.data;
+    },
+    function(error) {
+        if (!error.status) {
+            // Unhandled exception
+            console.error(error)
+            Vue.prototype.$toaster.error('networkError')
+            return Promise.reject(error.response);
+        }
 
-    return Promise.reject(error.response);
-  }
+        // Do something with response error
+        // In case of 401, redirect to login page
+        // If the error come from the login page, display the error
+        const errCode = error.response.data.error;
+        if(error.response.status === 401 && errCode === 'authNotAuthenticated') {
+            window.location = process.env.VUE_APP_LOGIN_PAGE
+        } else {
+            Vue.prototype.$toaster.error(error.response.data.error)
+        }
+
+        return Promise.reject(error.response);
+    }
 );
 
 Plugin.install = function(Vue) {
-  Vue.axios = _axios;
-  window.axios = _axios;
-  Object.defineProperties(Vue.prototype, {
-    $http: {
-      get() {
-        return _axios;
-      }
-    },
-    $axios: {
-      get() {
-        return _axios;
-      }
-    },
-  });
+    Vue.axios = _axios;
+    window.axios = _axios;
+    Object.defineProperties(Vue.prototype, {
+        $http: {
+            get() {
+                return _axios;
+            }
+        },
+        $axios: {
+            get() {
+                return _axios;
+            }
+        },
+    });
 };
 
 Vue.use(Plugin)
