258f45e37e1885db4c7670f2c787f30d363ac044
Nans Laupretre <nans.laupretre@gmail.com>
2021-03-08 19:33:16 +0100

    Add remote dropdown on Form.vue

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -32,6 +32,25 @@
         @click:append="state[field.name].show = !state[field.name].show",
         @keyup.enter="validate()"
       )
+      // Remote dropdown fields
+      v-select(
+        v-if="field.type === 'remote-dropdown'",
+        v-model="data[field.name]",
+        :chips="field.multiple",
+        :deletable-chips="field.multiple",
+        :multiple="field.multiple",
+        outlined,
+        clearable,
+        :menu-props="{ bottom: true, offsetY: true }",
+        :label="$t(field.label)",
+        :items="remoteItems[field.name] ? remoteItems[field.name].items : []",
+        :item-value="field.itemKey",
+        :item-text="field.itemValue",
+        :loading="!remoteItems[field.name] || !remoteItems[field.name].ready",
+        :disabled="!remoteItems[field.name] || !remoteItems[field.name].ready",
+        :rules="[rules[field.rule]]",
+        @keyup.enter="validate()"
+      )
 
   // Validate button
   v-btn.mt-3(
@@ -52,13 +71,22 @@ export default {
   name: "Form",
   data() {
     return {
+      /** initiliaze states for each fields */
       state: this.contract.reduce(
         (acc, cur) => ({ ...acc, [cur.name]: { show: false } }),
         0
       ),
+      /** availables fields rules */
       rules: {
-        required: (value) => !!value || this.$t("rules.required"),
+        required: (value) =>
+          (Array.isArray(value) && !!value.length) ||
+          (!Array.isArray(value) && !!value) ||
+          this.$t("rules.required"),
       },
+      /** form data to send */
+      data: {},
+      /** remote fields data */
+      remoteItems: {},
     };
   },
   props: {
@@ -68,22 +96,25 @@ export default {
         return { valid: false, loading: false };
       },
     },
-    data: {
-      type: Object,
-      default: () => {
-        return {};
-      },
-    },
     validateLabel: { type: String, default: "form.save" },
     validateWidth: { type: String, default: "150" },
     contract: { type: Array, required: true },
     href: { type: String, required: true },
     method: { type: String, required: true },
   },
+  mounted() {
+    this.fetchRemoteItems();
+  },
   methods: {
+    /**
+     * Tricks to update the v-model Object
+     */
     updateModel(key, value) {
       setImmediate(() => this.$emit("input", { ...this.value, [key]: value }));
     },
+    /**
+     * Validate forms and call API end point
+     */
     validate() {
       if (!this.value.valid) {
         this.$refs.form.validate();
@@ -102,6 +133,38 @@ export default {
           this.updateModel("loading", false);
           this.$emit("onError", err);
         });
+    },
+    /**
+     * Fetch items for remote fields if needed
+     */
+    fetchRemoteItems() {
+      this.contract.forEach((ctr) => {
+        if (ctr.href) {
+          this.$axios.get(ctr.href).then((response) => {
+            this.remoteItems[ctr.name] = {
+              items: response.data.list,
+              ready: true,
+            };
+            this.$forceUpdate();
+          });
+        }
+      });
+    },
+  },
+  watch: {
+    /**
+     * v-model: display property
+     * on false we reset the form
+     * on true, we force all remote fields to reload
+     */
+    "value.display": function (value) {
+      if (!value) {
+        this.$refs.form.reset();
+      } else {
+        this.remoteItems = {};
+        this.$forceUpdate();
+        this.fetchRemoteItems();
+      }
     },
   },
 };
