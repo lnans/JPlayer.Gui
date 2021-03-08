4b4b807ffc57848d4596e00de4013bddb1dd4b36
Nans Laupretre <nans.laupretre@gmail.com>
2021-03-08 19:35:02 +0100

    Add translation and CSS fix

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+v-card(width="100%")
+  v-card-title.primary.white--text.bold.d-flex.justify-space-between
+    p.ma-0 {{ title }}
+    slot(name="actions")
+  v-card-text.pa-0
+    slot(name="content")
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    title: { type: String, required: true }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
